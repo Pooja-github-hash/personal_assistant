e00a8fa05802ccb4684f8ecb84d05df1211175f6
poojaR <poojapooja.nila@gmail.com>
2024-12-12 16:23:16 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Personal Assistant',
+    version= '0.0.0',
+    author= 'Pooja R',
+    author_email= 'poojanila424@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
